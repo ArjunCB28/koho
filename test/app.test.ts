650ea60d199144a3f3b121615206ac6d8a4e70cd
ArjunCB28 <arjuncb28@gmail.com>
2020-07-20 16:24:40 -0300

    adding test cases

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,3 +1,13 @@
 import { expect } from 'chai';
-import {getWeekNumber } from '../src/app';
+import { getWeekNumber, getDayNumber } from '../src/app';
 
+describe('testing velocity limits', () => {
+    it('testing the week number method', () => {
+        const weekNumber: number = getWeekNumber("2020-07-20T00:00:00Z");
+        expect(weekNumber).to.equal(30);
+    });
+    it('testing the week number method', () => {
+        const dayNumber: number = getDayNumber("2020-07-20T00:00:00Z");
+        expect(dayNumber).to.equal(1);
+    });
+});
