4a33c3a04cc70f4295762e6be8089f4a64d7430c
ArjunCB28 <arjuncb28@gmail.com>
2020-07-20 23:19:28 -0300

    Added test cases to cover all business logic

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
-import fs from 'fs';
-import readline from 'readline';
-import moment from 'moment';
+import fs from 'fs'; // file read and write operations
+import readline from 'readline'; // read file line by line
+import moment from 'moment'; // date and time library
 
 // update momentjs locale to consider Monday as first day of the week
 moment.updateLocale('en', {week: {dow: 1,},});
 
+// transactionObj stores the transaction made on the customers. Customer data are stored as key value pairs with customer_id as key and weekly transaction as value
+// The weekly object has day number as key and array of transactions on that day as value.
+// this object structure will be very easy to traverse.
+// Importantly, no explicit find or filter method is used on this object to calculate daily or weekly transaction. The way it is constructed will be really easy traverse.
 const transactionObj = {};
-var outputString: string = "";
+
 const dailyLimit: number = 5000;
-const dailyLoadLimit: number = 3;
 const weeklyLimit: number = 20000;
+const dailyLoadLimit: number = 3;
+let outputString: string = '';
 
 // create line reader for the input.txt file using the fs file system and readline
 const lineReader: any = readline.createInterface({
@@ -17,61 +22,61 @@ const lineReader: any = readline.createInterface({
 });
 
 // get week of the year from 1970
-export const getWeekNumber = function (time): number {
+export const getWeekNumber = (time: string): number => {
 	return  moment.utc(time).week();
 };
 
 // get day of the week
-export const getDayNumber = function (time): number {
+export const getDayNumber = (time): number => {
 	return  moment.utc(time).day();
 };
 
 // function to add customer details, week and day into transaction object.
-const initCustomerDetails = function (loadJSON): void {
+const initCustomerDetails = (loadJSON): void => {
 	const transactionWeek = getWeekNumber(loadJSON.time);
 	const transactionDay = getDayNumber(loadJSON.time);
 	if (transactionObj[loadJSON.customer_id] === undefined) transactionObj[loadJSON.customer_id] = {id_set: []};
 	if (transactionObj[loadJSON.customer_id][transactionWeek] === undefined) transactionObj[loadJSON.customer_id][transactionWeek] = {};
 	if (transactionObj[loadJSON.customer_id][transactionWeek][transactionDay] === undefined) transactionObj[loadJSON.customer_id][transactionWeek][transactionDay] = [];
-	transactionObj[loadJSON.customer_id]["id_set"].push(loadJSON.id);
+	transactionObj[loadJSON.customer_id].id_set.push(loadJSON.id);
 };
 
 // function to store the output json data
-const outputJSON = function(loadId: string, customerId: string, accepted: boolean){
-	outputString += JSON.stringify({id: loadId, customer_id: customerId, accepted: accepted}) + "\n";
-	return {id: loadId, customer_id: customerId, accepted: accepted};
-}
+const outputJSON = (loadId: string, customerId: string, accepted: boolean) => {
+	outputString += JSON.stringify({id: loadId, customer_id: customerId, accepted}) + "\n";
+	return {id: loadId, customer_id: customerId, accepted};
+};
 
 // getAmount function returns back the load amount in integer after removing $ symbol
-export const getAmount = function (amount: string): number {
-	return parseInt(amount.split('$')[1].trim());
-}
+export const getAmount = (amount: string): number => {
+	return parseInt(amount.split('$')[1].trim(), 10);
+};
 
 // calculate sum of load amount for the day
-const calculateSumOfLoadAmount = function (loadAmount, transactionHistory): number{
+const calculateSumOfLoadAmount = (loadAmount, transactionHistory): number => {
 	let dailyLoadAmount = loadAmount;
 	transactionHistory.forEach((load) => {
 		dailyLoadAmount += getAmount(load.load_amount);
 	});
 	return dailyLoadAmount;
-}
+};
 
 // function to check the business logic
-const checkBusinessLogic = function(loadJSON): boolean{
+const checkBusinessLogic = (loadJSON): boolean => {
 	// ignore if load ID is observed more than once for a particular user
-	if (transactionObj[loadJSON.customer_id] !== undefined && transactionObj[loadJSON.customer_id]["id_set"].includes(loadJSON.id)) return null;
+	if (transactionObj[loadJSON.customer_id] !== undefined && transactionObj[loadJSON.customer_id].id_set.includes(loadJSON.id)) return null;
 	initCustomerDetails(loadJSON);
 	const transactionWeek = getWeekNumber(loadJSON.time);
 	const transactionDay = getDayNumber(loadJSON.time);
 	// if amount exceeds one day limit of 5000, return accepted as false
 	if (getAmount(loadJSON.load_amount) > dailyLimit) return false;
 	if (transactionObj[loadJSON.customer_id][transactionWeek][transactionDay].length >= dailyLoadLimit) return false;
-	let dailyLoadAmount = calculateSumOfLoadAmount(getAmount(loadJSON.load_amount), transactionObj[loadJSON.customer_id][transactionWeek][transactionDay]);
+	const dailyLoadAmount = calculateSumOfLoadAmount(getAmount(loadJSON.load_amount), transactionObj[loadJSON.customer_id][transactionWeek][transactionDay]);
 	// if daily transaction exceeds 5000, do not accept the load
 	if (dailyLoadAmount > dailyLimit) return false;
 	let weeklyLoadAmount = getAmount(loadJSON.load_amount);
-	for (var key in transactionObj[loadJSON.customer_id][transactionWeek]) {
-		weeklyLoadAmount = calculateSumOfLoadAmount(weeklyLoadAmount, transactionObj[loadJSON.customer_id][transactionWeek][key]);
+	for (const key in transactionObj[loadJSON.customer_id][transactionWeek]) {
+		if (transactionObj[loadJSON.customer_id][transactionWeek].hasOwnProperty(key)) weeklyLoadAmount = calculateSumOfLoadAmount(weeklyLoadAmount, transactionObj[loadJSON.customer_id][transactionWeek][key]);
 	}
 	// if weekly transaction exceeds 20000
 	if (weeklyLoadAmount > weeklyLimit) return false;
@@ -82,7 +87,7 @@ const checkBusinessLogic = function(loadJSON): boolean{
 // this function checks the velocity limit.
 // this function is exported so that it could be used for testing purposes.
 // checkVelocityLimit function is isolated from the business logic function
-export const checkVelocityLimit = function (loadJSON): boolean {
+export const checkVelocityLimit = (loadJSON): boolean => {
 	const isAcccepted = checkBusinessLogic(loadJSON);
 	if (isAcccepted !== null)  outputJSON(loadJSON.id, loadJSON.customer_id, isAcccepted);
 	return isAcccepted;
@@ -98,9 +103,3 @@ lineReader.on('close', ()=> {
 	fs.writeFileSync('dist/output.txt', outputString);
 	console.log("output.txt file created successfully");
 });
-
-// check variables and comments
-// testing
-// eslint
-// src dist input and output file
-// return type of json outputJSON function
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,13 +1,78 @@
 import { expect } from 'chai';
-import { getWeekNumber, getDayNumber } from '../src/app';
+import { getWeekNumber, getDayNumber, checkVelocityLimit } from '../src/app';
 
-describe('testing velocity limits', () => {
+describe('testing week and day calculation', () => {
     it('testing the week number method', () => {
         const weekNumber: number = getWeekNumber("2020-07-20T00:00:00Z");
         expect(weekNumber).to.equal(30);
     });
-    it('testing the week number method', () => {
+    it('testing the days number method', () => {
         const dayNumber: number = getDayNumber("2020-07-20T00:00:00Z");
         expect(dayNumber).to.equal(1);
     });
 });
+
+describe('testing velocity limit', () => {
+    it('single transaction lesser than 5000 should be accepted', () => {
+        const isAccepted = checkVelocityLimit({"id":"10000","customer_id":"100","load_amount":"$3000","time":"2000-01-01T00:00:00Z"});
+        expect(isAccepted).to.be.true;
+    });
+
+    it('sum of amount loaded in a single day lesser than 5000 should be accepted ', () => {
+        checkVelocityLimit({"id":"10001","customer_id":"101","load_amount":"$2000.00","time":"2000-01-01T00:00:00Z"});
+        checkVelocityLimit({"id":"10002","customer_id":"101","load_amount":"$2000.00","time":"2000-01-01T00:00:00Z"})
+        const isAccepted = checkVelocityLimit({"id":"10003","customer_id":"101","load_amount":"$500.00","time":"2000-01-01T00:00:00Z"})
+        expect(isAccepted).to.be.true;
+    });
+
+    it('sum of amount loaded in a week lesser than 20000 should be accepted', () => {
+        checkVelocityLimit({"id":"10010","customer_id":"110","load_amount":"$4000.00","time":"2020-07-20T00:00:00Z"});
+        checkVelocityLimit({"id":"10011","customer_id":"110","load_amount":"$4000.00","time":"2020-07-21T00:00:00Z"})
+        const isAccepted = checkVelocityLimit({"id":"10012","customer_id":"110","load_amount":"$5000.00","time":"2020-07-23T00:00:00Z"})
+        expect(isAccepted).to.be.true;
+    });
+
+    it('loading an amount greater than 5000 should NOT be accepted', () => {
+        const isAccepted = checkVelocityLimit({"id":"10020","customer_id":"120","load_amount":"$6000","time":"2000-01-01T00:00:00Z"});
+        expect(isAccepted).to.be.false;
+    });
+
+    it('sum of amount loaded in a single day exceeds 5000 should NOT be accepted', () => {
+        checkVelocityLimit({"id":"10030","customer_id":"130","load_amount":"$2000.00","time":"2000-01-01T00:00:00Z"});
+        checkVelocityLimit({"id":"10031","customer_id":"130","load_amount":"$2000.00","time":"2000-01-01T00:00:00Z"})
+        const isAccepted = checkVelocityLimit({"id":"10032","customer_id":"130","load_amount":"$2000.00","time":"2000-01-01T00:00:00Z"})
+        expect(isAccepted).to.be.false;
+    });
+
+    it('loading more than 3 times a day should fail', () => {
+        checkVelocityLimit({"id":"10070","customer_id":"170","load_amount":"$1000.00","time":"2000-01-01T00:00:00Z"});
+        checkVelocityLimit({"id":"10071","customer_id":"170","load_amount":"$1000.00","time":"2000-01-01T00:00:00Z"})
+        checkVelocityLimit({"id":"10072","customer_id":"170","load_amount":"$1000.00","time":"2000-01-01T00:00:00Z"})
+        const isAccepted = checkVelocityLimit({"id":"10073","customer_id":"170","load_amount":"$500.00","time":"2000-01-01T00:00:00Z"})
+        expect(isAccepted).to.be.false;
+    });
+
+    it('sum of amount loaded in a week is exceeds 20000 should NOT be accepted', () => {
+        checkVelocityLimit({"id":"10040","customer_id":"140","load_amount":"$5000.00","time":"2020-07-20T00:00:00Z"});
+        checkVelocityLimit({"id":"10041","customer_id":"140","load_amount":"$5000.00","time":"2020-07-21T00:00:00Z"})
+        checkVelocityLimit({"id":"10042","customer_id":"140","load_amount":"$5000.00","time":"2020-07-22T00:00:00Z"})
+        const isAccepted = checkVelocityLimit({"id":"10043","customer_id":"140","load_amount":"$5000.00","time":"2020-07-23T00:00:00Z"})
+        expect(isAccepted).to.be.true;
+    });
+
+    it('Monday is the starting day of the week.', () => {
+        checkVelocityLimit({"id":"10050","customer_id":"150","load_amount":"$5000.00","time":"2020-07-19T00:00:00Z"});
+        checkVelocityLimit({"id":"10051","customer_id":"150","load_amount":"$5000.00","time":"2020-07-20T00:00:00Z"});
+        checkVelocityLimit({"id":"10052","customer_id":"150","load_amount":"$5000.00","time":"2020-07-21T00:00:00Z"})
+        checkVelocityLimit({"id":"10053","customer_id":"150","load_amount":"$5000.00","time":"2020-07-22T00:00:00Z"})
+        const isAccepted = checkVelocityLimit({"id":"10054","customer_id":"150","load_amount":"$5000.00","time":"2020-07-23T00:00:00Z"})
+        expect(isAccepted).to.be.true;
+    });
+
+    it('Ignore upcoming transaction with same load id for a particular customer', () => {
+        checkVelocityLimit({"id":"10060","customer_id":"160","load_amount":"$1000.00","time":"2020-07-19T00:00:00Z"});
+        const isAccepted = checkVelocityLimit({"id":"10060","customer_id":"160","load_amount":"$1000.00","time":"2020-07-20T00:00:00Z"});
+        expect(isAccepted).to.equal(null);
+    });
+});
+
